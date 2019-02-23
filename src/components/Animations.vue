1617c12e8c8df04925925294e1dd724a643ddb40
Román Martínez <roman.mx3@gmail.com>
2019-02-23 15:13:00 -0600

    Added info about extra settings to Animations.vue

--- src/components/Animations.vue
+++ src/components/Animations.vue
@@ -27,11 +27,56 @@
 			<li><code>v-appear-to</code></li>
 			<li><code>v-move</code>: set when animating position changes of elements within a &lt;transition-group&gt;</li>
 		</ul>
-		<h3>Examples of basic animations</h3>
+		<h3>Examples of default animations</h3>
 		<ul>
-			<li>Single element: <transition><strong v-if="toggle">Element with transition!</strong></transition></li>
-			<li>Multiple elements: <transition-group class="transition-group"><strong v-if="toggle" key="yes">Yes!</strong><strong v-else key="no">No!</strong></transition-group></li>
+			<li>Single element:
+				<transition>
+					<strong v-if="toggle">Element with transition!</strong>
+				</transition>
+			</li>
+			<li>Multiple elements:
+				<transition-group>
+					<strong v-if="toggle" key="yes">Yes!</strong>
+					<strong v-else key="no">No!</strong>
+				</transition-group>
+			</li>
 		</ul>
+		<p><button class="button" @click="toggle = !toggle">Toggle elements</button></p>
+		
+		<hr>
+		<h2>Extra settings</h2>
+		<ul>
+			<li><code>name</code> changes the class names to be added on each animation step, e.g. setting <code>name="fade"</code> will add classes like <code>fade-enter-active</code></li>
+			<li><code>mode</code> defines whether to animate both "enter" and "leave" items at the same time, or to do it sequentially with <code>mode="in-out"</code> or <code>mode="out-in"</code>. <strong>NOTE:</strong> this is not available on the <code>&lt;transition-group&gt;</code> tag</li>
+			<li><code>enter-class</code>, <code>enter-active-class</code>, etc… lets you define different CSS classes for each animation step instead of the ones generated automatically</li>
+			<li><code>appear</code> if set to true, applies transitions to the element on initial render. defaults to false</li>
+			<li><code>css</code> defines whether or not to apply the CSS classes to the element. defaults to true</li>
+			<li><code>type</code> defines what css property to look for to determine the total length of the transitions, options are <code>transition</code> and <code>animation</code>, or you can pass a number to set it manually. defaults to the css property with the longest duration</li>
+		</ul>
+		<h3>Examples of custom animations</h3>
+		<ul>
+			<li><code>name="slide"</code>:
+				<transition-group name="slide">
+					<strong v-if="toggle" key="yes">Yes!</strong>
+					<strong v-else key="no">No!</strong>
+				</transition-group>
+			</li>
+			<li><code>name="slide" mode="out-in"</code>:
+				<span>
+					<transition name="slide" mode="out-in">
+						<strong v-if="toggle" key="yes">Yes!</strong>
+						<strong v-else key="no">No!</strong>
+					</transition>
+				</span>
+			</li>
+			<li><code>name="slide" appear</code>:
+				<transition-group name="slide" appear>
+					<strong v-if="toggle" key="yes">Yes!</strong>
+					<strong v-else key="no">No!</strong>
+				</transition-group>
+			</li>
+		</ul>
+		<p><button class="button" @click="toggle = !toggle">Toggle elements</button></p>
 	</div>
 </template>
 
@@ -48,17 +93,18 @@ export default {
 
 <style lang="scss">
 	
-	// Default animations
+	// Default animation
 	// ------------------------------
+	// Container
 	span > .v-leave-active {
 		position: absolute;
 	}
 	// Enter
-	.v-enter-active { transition: all 1s }
-	.v-enter { opacity: 0; transform: scale( 2 ) }
+	.v-enter-active { transition: opacity 1s }
+	.v-enter { opacity: 0 }
 	.v-enter-to {}
 	// Leave
-	.v-leave-active { transition: all 2s }
+	.v-leave-active { transition: opacity 2s }
 	.v-leave {}
 	.v-leave-to { opacity: 0 }
 	// Appear
@@ -67,5 +113,26 @@ export default {
 	.v-appear-to {}
 	// Move
 	.v-move {}
+	
+	// Slide animation
+	// ------------------------------
+	// Container
+	span > .slide-leave-active {
+		position: absolute;
+	}
+	// Enter
+	.slide-enter-active { display: inline-block; transition: opacity 1s, transform 1s; }
+	.slide-enter { opacity: 0; transform: translateX( -10rem ) }
+	.slide-enter-to {}
+	// Leave
+	.slide-leave-active { display: inline-block; transition: opacity 2s, transform 2s; }
+	.slide-leave {}
+	.slide-leave-to { opacity: 0; transform: translateX( 10rem ) }
+	// Appear
+	.slide-appear-active {}
+	.slide-appear {}
+	.slide-appear-to {}
+	// Move
+	.slide-move {}
 	
 </style>
